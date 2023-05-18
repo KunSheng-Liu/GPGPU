01e2c766a0a27fd0d5b2e2642650f92b4ee384c2
KunSheng <M11007412@mail.ntust.edu.tw>
2023-05-18 18:38:49 +0800

    Add thread executing tool
    Detial:
            * Add ./src/running.cpp
            * Add taskset_list.txt
            * Remove run.sh
            * Revise Makefile

--- Makefile
+++ Makefile
@@ -1,35 +1,25 @@
 CXX 			:= g++
-SRC 			:= $(wildcard ./src/*.cpp)
+SRC 			:= $(filter-out ./src/running.cpp, $(wildcard ./src/*.cpp))
 OBJ				:= $(patsubst %.cpp, %.o, $(SRC))
 CXXFLAGS 		:= -std=c++17 -pipe -g -O3
-SHARED_LIBRARY 	=
+SHARED_LIBRARY 	:= -pthread
 
-# Add needed header path
-# SHARED_LIBRARY += -I/usr/local/cuda-11.4/include
-
-# Add dynamic library
-SHARED_LIBRARY += -pthread
-# SHARED_LIBRARY += -lonnxruntime
-# SHARED_LIBRARY += -lopencv_core -lopencv_imgproc -lopencv_imgcodecs -lopencv_dnn
-# SHARED_LIBRARY += -L/usr/local/cuda/lib64 -lcuda -lcudart
-
-
-GPGPU: update $(OBJ)
+GPGPU: $(OBJ)
 	@echo Compiling GPGPU
 	@$(CXX) $(CXXFLAGS) -o GPGPU $(OBJ) $(SHARED_LIBRARY)
 
+RUN:
+	@echo Compiling RUN
+	@$(CXX) $(CXXFLAGS) -o RUN ./src/running.cpp $(SHARED_LIBRARY)
 
 %.o: %.cpp
 	@echo Build $@
 	@@$(CXX) $(CXXFLAGS) -c -o $@ $<
-#@@$(CXX) $(CXXFLAGS) -MMD -MP -c -o $@ $<
 
-.PHONY: update clean
+.PHONY: update clean RUN
 update:
 	@:
 
 clean:
 	@find -name "*.o" -exec rm {} \;
-#@find -name "*.d" -exec rm {} \;
-
 
---
+++ src/running.cpp
@@ -0,0 +1,108 @@
+/**
+ * \name    Running.cpp
+ * 
+ * \brief   It's a thread running tool
+ * 
+ * \date    May 18, 2023
+ */
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include <semaphore.h>
+
+/* ************************************************************************************************
+ * Name Space
+ * ************************************************************************************************
+ */
+using namespace ;
+
+/* ************************************************************************************************
+ * Software Configuration
+ * ************************************************************************************************
+ */
+#define THREAD_NUM_LIMIT 4
+
+/* ************************************************************************************************
+ * Function Declaration
+ * ************************************************************************************************
+ */
+void exe_thread(string cmd);
+
+/* ************************************************************************************************
+ * Global Resource
+ * ************************************************************************************************
+ */
+sem_t* count_semaphore = new sem_t;
+
+/** ===============================================================================================
+ * \name    main
+ * 
+ * \brief   Thread executing the command inside the \b ./taskset_list.txt file
+ * 
+ * \param   numOfThread    the maximum parallel thread can execute
+ * 
+ * \note    can ignore the command by add "//" in the start of line
+ * 
+ * \endcond
+ * ================================================================================================
+ */
+int main(int argc, char** argv)
+{
+    vector<string> task_list;
+    
+    /* Load the file */
+    fstream file;   
+    file.open("./taskset_list.txt", ios::in); 
+    if (!file.is_open()) {cout << "ERROR Can't open file\n"; abort();}
+
+    /* Parser vaild command */
+    string readLine;
+    while (getline(file, readLine))
+    {
+        if (!(readLine.find("//") == 0 || readLine.empty())) task_list.push_back(readLine);
+    }
+
+    /* Determine the parallel ability */
+    int thread_num = (argc > 1) ? atoi(argv[1]) : THREAD_NUM_LIMIT;
+    sem_init(count_semaphore, 0, thread_num);
+
+    /* Launch tasks */
+    thread threads[task_list.size()];
+    for (int i = 0; i < task_list.size(); i++)
+    {
+        threads[i] = thread(exe_thread, task_list.at(i));
+    }
+
+    /* Waiting thread finish */
+    for (int i = 0; i < task_list.size(); i++)
+    {
+        threads[i].join();
+    }
+    
+    sem_destroy(count_semaphore);
+}
+
+
+/** ===============================================================================================
+ * \name    exe_thread
+ * 
+ * \brief   Thread executing the command once the thread pend the semaphore
+ * 
+ * \param   cmd    the command going to execute
+ * 
+ * \endcond
+ * ================================================================================================
+ */
+void exe_thread(string cmd)
+{
+    sem_wait(count_semaphore);
+        cout << "Exec cmd: " << cmd << endl;
+        int ret = system(cmd.c_str());
+    sem_post(count_semaphore);
+
+    cout << "Finish: " << cmd << endl;
+}
